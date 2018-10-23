0e4096a63fc6d714aedf4e9a3d30680d8a1e6c52
Jason Guest <jason@rgrmarketing.com>
2018-10-22 20:53:43 -0700

    Added foreach and modified array

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <?php
 
-	require ("player.php");
+	// require ("player.php");
 
-	$jason = new Player();
+	// $jason = new Player();
 	
-	echo "here is the health points before initializing: " . $jason->healthPoints;
+	// echo "here is the health points before initializing: " . $jason->healthPoints;
 	
-	$jason->healthPoints = 100;
+	// $jason->healthPoints = 100;
 	
-	echo "<br />";
-	echo "health before damage: " . $jason->healthPoints;
-	echo "<br />";
-	echo "health after damage: " . $jason->setRandomDamage();
+	// echo "<br />";
+	// echo "health before damage: " . $jason->healthPoints;
+	// echo "<br />";
+	// echo "health after damage: " . $jason->setRandomDamage();
+
+
+	$items = 
+	[
+		"Estus Flask" => 20,
+	];
+
+	foreach ($items as $item => $value) 
+	{
+		if ($item == "Estus Flask") 
+		{
+			echo "You have an estus flask!";
+		} else 
+		{
+			echo "You don't have an estus flask";
+		}
+	}
+
+
+
 
 ?>
